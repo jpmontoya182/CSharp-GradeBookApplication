06d16fe9ac768a3091acd00bebdb73a3a19da1bb
Juan Pablo Montoya Cardona <jpmontoya182@gmail.com>
2021-04-13 09:56:31 -0500

    it was finished the test

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
@@ -16,8 +17,26 @@ namespace GradeBook.GradeBooks
             {
                 throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
             }
-           
-            return 'F';        
+
+            var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+            var grades = Students
+                    .OrderByDescending(e => e.AverageGrade)
+                    .Select(e => e.AverageGrade)
+                    .ToList();
+
+            if (averageGrade >= grades[threshold - 1])
+            { 
+                return 'A';
+            }
+
+            if (averageGrade >= grades[(threshold * 2) - 1])
+                return 'B';
+            if (averageGrade >= grades[(threshold * 3) - 1])
+                return 'C';
+            if (averageGrade >= grades[(threshold * 4) - 1])
+                return 'D';
+            return 'F';
+    
         }
     }
 }
